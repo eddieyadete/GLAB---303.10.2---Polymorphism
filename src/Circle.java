b0317aa052e3ec0348bc87c10153643ce853fa5b
eddie yadete <eddieyadete123@gmail.com>
2024-12-08 15:45:56 -0600

    GLAB---303.10.2---Polymorphism.git

---
+++ src/Circle.java
@@ -0,0 +1,28 @@
+public class Circle extends Shape{
+
+    protected double radius;
+
+    private final double PI = Math.PI;
+
+    public Circle(double radius){
+
+        this.radius = radius;
+    }
+    public Circle(double radius, double height){
+        this.radius = radius;
+        this.height = height;
+    }
+    public double getArea(){
+        super.area = PI * Math.pow(this.radius, 2);
+        return super.area;
+    }
+    @Override
+    public void displayShapeName(){
+        System.out.println("Drawing a circle of radius " + this.radius);
+    }
+    @Override
+    public String toString() {
+        return "Circle[ radius = " + radius + super.toString() + "]";
+    }
+
+}
